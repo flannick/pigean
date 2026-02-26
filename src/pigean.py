da43b997e37ff1cd7114781f670a2f4f8ca34670
Jason Flannick <flannick@broadinstitute.org>
2026-02-26 11:16:27 -0500

    refactor(gibbs): simplify epoch iteration API using epoch/phase contexts

--- src/pigean.py
+++ src/pigean.py
@@ -17548,84 +17548,10 @@ def _run_gibbs_epoch_phase(
         epoch_loop_update = _run_gibbs_epoch_iterations(
             state=state,
             run_state=run_state,
-            epoch_control=epoch_control,
-            epoch_sums=epoch_sums,
-            epoch_priors=epoch_priors,
-            epoch_runtime=epoch_runtime,
-            epoch_total_iter_offset=epoch_total_iter_offset,
-            epoch_max_num_iter=epoch_max_num_iter,
-            min_num_iter_for_epoch=min_num_iter_for_epoch,
-            min_num_burn_in_for_epoch=min_num_burn_in_for_epoch,
-            max_num_burn_in_for_epoch=max_num_burn_in_for_epoch,
-            min_num_post_burn_in_for_epoch=min_num_post_burn_in_for_epoch,
-            max_num_post_burn_in_for_epoch=max_num_post_burn_in_for_epoch,
-            num_full_gene_sets=num_full_gene_sets,
-            num_chains=num_chains,
-            full_betas_m_shape=full_betas_m_shape,
-            num_stack_batches=num_stack_batches,
-            stack_batch_size=stack_batch_size,
-            X_hstacked=X_hstacked,
-            gauss_seidel=gauss_seidel,
-            eps=eps,
-            diag_every=diag_every,
-            active_beta_top_k=active_beta_top_k,
-            active_beta_min_abs=active_beta_min_abs,
-            burn_in_rhat_quantile=burn_in_rhat_quantile,
-            r_threshold_burn_in=r_threshold_burn_in,
-            burn_in_patience=burn_in_patience,
-            stop_patience=stop_patience,
-            stop_mcse_quantile=stop_mcse_quantile,
-            beta_rel_mcse_denom_floor=beta_rel_mcse_denom_floor,
-            stop_top_gene_k=stop_top_gene_k,
-            stop_min_gene_d=stop_min_gene_d,
-            max_rel_mcse_beta=max_rel_mcse_beta,
-            max_abs_mcse_d=max_abs_mcse_d,
-            stall_window=stall_window,
-            stall_min_burn_in=stall_min_burn_in,
-            stall_min_post_burn_in=stall_min_post_burn_in,
-            stall_delta_rhat=stall_delta_rhat,
-            stall_delta_mcse=stall_delta_mcse,
-            stall_recent_window=stall_recent_window,
-            stall_recent_eps=stall_recent_eps,
-            burn_in_stall_window=burn_in_stall_window,
-            burn_in_stall_delta=burn_in_stall_delta,
-            post_burn_reset_arrays=post_burn_reset_arrays,
-            post_burn_reset_missing_arrays=post_burn_reset_missing_arrays,
-            cur_background_log_bf_v=cur_background_log_bf_v,
-            y_var_orig=y_var_orig,
-            initial_linear_filter=initial_linear_filter,
-            sparse_frac_gibbs=sparse_frac_gibbs,
-            sparse_max_gibbs=sparse_max_gibbs,
-            pre_filter_batch_size=pre_filter_batch_size,
-            pre_filter_small_batch_size=pre_filter_small_batch_size,
-            passed_in_max_num_burn_in=passed_in_max_num_burn_in,
-            max_num_iter_betas=max_num_iter_betas,
-            min_num_iter_betas=min_num_iter_betas,
-            num_chains_betas=num_chains_betas,
-            r_threshold_burn_in_betas=r_threshold_burn_in_betas,
-            use_max_r_for_convergence_betas=use_max_r_for_convergence_betas,
-            max_frac_sem_betas=max_frac_sem_betas,
-            max_allowed_batch_correlation=max_allowed_batch_correlation,
-            gauss_seidel_betas=gauss_seidel_betas,
-            sparse_solution=sparse_solution,
-            sparse_frac_betas=sparse_frac_betas,
-            correct_betas_mean=correct_betas_mean,
-            correct_betas_var=correct_betas_var,
-            num_batches_parallel=num_batches_parallel,
-            warm_start=warm_start,
-            use_mean_betas=use_mean_betas,
-            update_huge_scores=update_huge_scores,
-            compute_Y_raw=compute_Y_raw,
-            betas_trace_out=betas_trace_out,
-            debug_zero_sparse=debug_zero_sparse,
-            adjust_priors=adjust_priors,
-            increase_hyper_if_betas_below_for_epoch=increase_hyper_if_betas_below_for_epoch,
-            num_mad=num_mad,
-            num_before_checking_p_increase=num_before_checking_p_increase,
-            p_scale_factor=p_scale_factor,
+            epoch_context=epoch_context,
+            phase_kwargs=phase_kwargs,
             gene_set_stats_trace_fh=gene_set_stats_trace_fh,
             gene_stats_trace_fh=gene_stats_trace_fh,
-            trace_chain_offset=trace_chain_offset,
             log_bf_m=log_bf_m,
             log_bf_uncorrected_m=log_bf_uncorrected_m,
             log_bf_raw_m=log_bf_raw_m,
@@ -17834,88 +17760,92 @@ def _finalize_gibbs_run_after_epochs(run_state, num_chains):
 def _run_gibbs_epoch_iterations(
     state,
     run_state,
-    epoch_control,
-    epoch_sums,
-    epoch_priors,
-    epoch_runtime,
-    epoch_total_iter_offset,
-    epoch_max_num_iter,
-    min_num_iter_for_epoch,
-    min_num_burn_in_for_epoch,
-    max_num_burn_in_for_epoch,
-    min_num_post_burn_in_for_epoch,
-    max_num_post_burn_in_for_epoch,
-    num_full_gene_sets,
-    num_chains,
-    full_betas_m_shape,
-    num_stack_batches,
-    stack_batch_size,
-    X_hstacked,
-    gauss_seidel,
-    eps,
-    diag_every,
-    active_beta_top_k,
-    active_beta_min_abs,
-    burn_in_rhat_quantile,
-    r_threshold_burn_in,
-    burn_in_patience,
-    stop_patience,
-    stop_mcse_quantile,
-    beta_rel_mcse_denom_floor,
-    stop_top_gene_k,
-    stop_min_gene_d,
-    max_rel_mcse_beta,
-    max_abs_mcse_d,
-    stall_window,
-    stall_min_burn_in,
-    stall_min_post_burn_in,
-    stall_delta_rhat,
-    stall_delta_mcse,
-    stall_recent_window,
-    stall_recent_eps,
-    burn_in_stall_window,
-    burn_in_stall_delta,
-    post_burn_reset_arrays,
-    post_burn_reset_missing_arrays,
-    cur_background_log_bf_v,
-    y_var_orig,
-    initial_linear_filter,
-    sparse_frac_gibbs,
-    sparse_max_gibbs,
-    pre_filter_batch_size,
-    pre_filter_small_batch_size,
-    passed_in_max_num_burn_in,
-    max_num_iter_betas,
-    min_num_iter_betas,
-    num_chains_betas,
-    r_threshold_burn_in_betas,
-    use_max_r_for_convergence_betas,
-    max_frac_sem_betas,
-    max_allowed_batch_correlation,
-    gauss_seidel_betas,
-    sparse_solution,
-    sparse_frac_betas,
-    correct_betas_mean,
-    correct_betas_var,
-    num_batches_parallel,
-    warm_start,
-    use_mean_betas,
-    update_huge_scores,
-    compute_Y_raw,
-    betas_trace_out,
-    debug_zero_sparse,
-    adjust_priors,
-    increase_hyper_if_betas_below_for_epoch,
-    num_mad,
-    num_before_checking_p_increase,
-    p_scale_factor,
+    epoch_context,
+    phase_kwargs,
     gene_set_stats_trace_fh,
     gene_stats_trace_fh,
-    trace_chain_offset,
     log_bf_m,
     log_bf_uncorrected_m,
     log_bf_raw_m,
 ):
+    epoch_control = epoch_context["epoch_control"]
+    epoch_sums = epoch_context["epoch_sums"]
+    epoch_priors = epoch_context["epoch_priors"]
+    epoch_runtime = epoch_context["epoch_runtime"]
+    epoch_total_iter_offset = epoch_context["epoch_total_iter_offset"]
+    epoch_max_num_iter = epoch_context["epoch_max_num_iter"]
+    min_num_iter_for_epoch = epoch_context["min_num_iter_for_epoch"]
+    min_num_burn_in_for_epoch = epoch_context["min_num_burn_in_for_epoch"]
+    max_num_burn_in_for_epoch = epoch_context["max_num_burn_in_for_epoch"]
+    min_num_post_burn_in_for_epoch = epoch_context["min_num_post_burn_in_for_epoch"]
+    max_num_post_burn_in_for_epoch = epoch_context["max_num_post_burn_in_for_epoch"]
+    full_betas_m_shape = epoch_context["full_betas_m_shape"]
+    num_stack_batches = epoch_context["num_stack_batches"]
+    stack_batch_size = epoch_context["stack_batch_size"]
+    X_hstacked = epoch_context["X_hstacked"]
+    trace_chain_offset = epoch_context["trace_chain_offset"]
+    increase_hyper_if_betas_below_for_epoch = epoch_context["increase_hyper_if_betas_below_for_epoch"]
+    num_before_checking_p_increase = epoch_context["num_before_checking_p_increase"]
+    p_scale_factor = epoch_context["p_scale_factor"]
+    post_burn_reset_arrays = epoch_context["post_burn_reset_arrays"]
+    post_burn_reset_missing_arrays = epoch_context["post_burn_reset_missing_arrays"]
+
+    num_full_gene_sets = phase_kwargs["num_full_gene_sets"]
+    num_chains = phase_kwargs["num_chains"]
+    gauss_seidel = phase_kwargs["gauss_seidel"]
+    eps = phase_kwargs["eps"]
+    diag_every = phase_kwargs["diag_every"]
+    active_beta_top_k = phase_kwargs["active_beta_top_k"]
+    active_beta_min_abs = phase_kwargs["active_beta_min_abs"]
+    burn_in_rhat_quantile = phase_kwargs["burn_in_rhat_quantile"]
+    r_threshold_burn_in = phase_kwargs["r_threshold_burn_in"]
+    burn_in_patience = phase_kwargs["burn_in_patience"]
+    stop_patience = phase_kwargs["stop_patience"]
+    stop_mcse_quantile = phase_kwargs["stop_mcse_quantile"]
+    beta_rel_mcse_denom_floor = phase_kwargs["beta_rel_mcse_denom_floor"]
+    stop_top_gene_k = phase_kwargs["stop_top_gene_k"]
+    stop_min_gene_d = phase_kwargs["stop_min_gene_d"]
+    max_rel_mcse_beta = phase_kwargs["max_rel_mcse_beta"]
+    max_abs_mcse_d = phase_kwargs["max_abs_mcse_d"]
+    stall_window = phase_kwargs["stall_window"]
+    stall_min_burn_in = phase_kwargs["stall_min_burn_in"]
+    stall_min_post_burn_in = phase_kwargs["stall_min_post_burn_in"]
+    stall_delta_rhat = phase_kwargs["stall_delta_rhat"]
+    stall_delta_mcse = phase_kwargs["stall_delta_mcse"]
+    stall_recent_window = phase_kwargs["stall_recent_window"]
+    stall_recent_eps = phase_kwargs["stall_recent_eps"]
+    burn_in_stall_window = phase_kwargs["burn_in_stall_window"]
+    burn_in_stall_delta = phase_kwargs["burn_in_stall_delta"]
+    cur_background_log_bf_v = phase_kwargs["cur_background_log_bf_v"]
+    y_var_orig = phase_kwargs["y_var_orig"]
+    initial_linear_filter = phase_kwargs["initial_linear_filter"]
+    sparse_frac_gibbs = phase_kwargs["sparse_frac_gibbs"]
+    sparse_max_gibbs = phase_kwargs["sparse_max_gibbs"]
+    pre_filter_batch_size = phase_kwargs["pre_filter_batch_size"]
+    pre_filter_small_batch_size = phase_kwargs["pre_filter_small_batch_size"]
+    passed_in_max_num_burn_in = phase_kwargs["passed_in_max_num_burn_in"]
+    max_num_iter_betas = phase_kwargs["max_num_iter_betas"]
+    min_num_iter_betas = phase_kwargs["min_num_iter_betas"]
+    num_chains_betas = phase_kwargs["num_chains_betas"]
+    r_threshold_burn_in_betas = phase_kwargs["r_threshold_burn_in_betas"]
+    use_max_r_for_convergence_betas = phase_kwargs["use_max_r_for_convergence_betas"]
+    max_frac_sem_betas = phase_kwargs["max_frac_sem_betas"]
+    max_allowed_batch_correlation = phase_kwargs["max_allowed_batch_correlation"]
+    gauss_seidel_betas = phase_kwargs["gauss_seidel_betas"]
+    sparse_solution = phase_kwargs["sparse_solution"]
+    sparse_frac_betas = phase_kwargs["sparse_frac_betas"]
+    correct_betas_mean = phase_kwargs["correct_betas_mean"]
+    correct_betas_var = phase_kwargs["correct_betas_var"]
+    num_batches_parallel = phase_kwargs["num_batches_parallel"]
+    warm_start = phase_kwargs["warm_start"]
+    use_mean_betas = phase_kwargs["use_mean_betas"]
+    update_huge_scores = phase_kwargs["update_huge_scores"]
+    compute_Y_raw = phase_kwargs["compute_Y_raw"]
+    betas_trace_out = phase_kwargs["betas_trace_out"]
+    debug_zero_sparse = phase_kwargs["debug_zero_sparse"]
+    adjust_priors = phase_kwargs["adjust_priors"]
+    num_mad = phase_kwargs["num_mad"]
+
     for iteration_num in range(epoch_max_num_iter):
         iteration_prepare = _prepare_gibbs_iteration_state(
             state=state,
