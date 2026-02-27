fb9530beaee199b4112ec36d85ceb5805b36976d
Jason Flannick <flannick@broadinstitute.org>
2026-02-27 17:01:55 -0500

    refactor(huge): extract regional conditional-probability helper

--- src/pigean.py
+++ src/pigean.py
@@ -4316,6 +4316,21 @@ class GeneSetData(object):
         cond_prob[cond_prob < max_log] = np.exp(cond_prob[cond_prob < max_log])
         return cond_prob
 
+    def _compute_huge_region_conditional_probabilities(self, var_log_bf, var_log_bf_detect, region_vars, max_log):
+        # Use log-sum-exp for numerical stability when normalizing regional variant Bayes factors.
+        c = np.max(var_log_bf[region_vars])
+        c_detect = np.max(var_log_bf_detect[region_vars])
+
+        log_sum_bf = c + np.log(np.sum(np.exp(var_log_bf[region_vars] - c)))
+        log_sum_bf_detect = c_detect + np.log(np.sum(np.exp(var_log_bf_detect[region_vars] - c_detect)))
+
+        log_rel_bf = var_log_bf[region_vars] - log_sum_bf
+        log_rel_bf_detect = var_log_bf_detect[region_vars] - log_sum_bf_detect
+
+        cond_prob = self._convert_huge_log_rel_bf_to_cond_prob(log_rel_bf, max_log)
+        cond_prob_detect = self._convert_huge_log_rel_bf_to_cond_prob(log_rel_bf_detect, max_log)
+        return (cond_prob, cond_prob_detect)
+
     def calculate_huge_scores_gwas(self, gwas_in, gwas_chrom_col=None, gwas_pos_col=None, gwas_p_col=None, gene_loc_file=None, hold_out_chrom=None, exons_loc_file=None, gwas_beta_col=None, gwas_se_col=None, gwas_n_col=None, gwas_n=None, gwas_freq_col=None, gwas_filter_col=None, gwas_filter_value=None, gwas_locus_col=None, gwas_ignore_p_threshold=None, gwas_units=None, gwas_low_p=5e-8, gwas_high_p=1e-2, gwas_low_p_posterior=0.98, gwas_high_p_posterior=0.001, detect_low_power=None, detect_high_power=None, detect_adjust_huge=False, learn_window=False, closest_gene_prob=0.7, max_closest_gene_prob=0.9, scale_raw_closest_gene=True, cap_raw_closest_gene=False, cap_region_posterior=True, scale_region_posterior=False, phantom_region_posterior=False, allow_evidence_of_absence=False, correct_huge=True, max_signal_p=1e-5, signal_window_size=250000, signal_min_sep=100000, signal_max_logp_ratio=None, credible_set_span=25000, max_closest_gene_dist=2.5e5, min_n_ratio=0.5, max_clump_ld=0.2, min_var_posterior=0.01, s2g_in=None, s2g_chrom_col=None, s2g_pos_col=None, s2g_gene_col=None, s2g_prob_col=None, s2g_normalize_values=None, credible_sets_in=None, credible_sets_id_col=None, credible_sets_chrom_col=None, credible_sets_pos_col=None, credible_sets_ppa_col=None, **kwargs):
         (signal_window_size, signal_max_logp_ratio) = _validate_and_normalize_huge_gwas_inputs(
             gwas_in=gwas_in,
@@ -5261,20 +5276,12 @@ class GeneSetData(object):
 
 
                         if cond_prob is None:
-                            #now find the conditional posteriors of all of the variants in the region
-                            #use log sum exp trick
-
-                            c = np.max(var_log_bf[region_vars])
-                            c_detect = np.max(var_log_bf_detect[region_vars])
-
-                            log_sum_bf = c + np.log(np.sum(np.exp(var_log_bf[region_vars] - c)))
-                            log_sum_bf_detect = c_detect + np.log(np.sum(np.exp(var_log_bf_detect[region_vars] - c_detect)))
-
-                            log_rel_bf = var_log_bf[region_vars] - log_sum_bf
-                            log_rel_bf_detect = var_log_bf_detect[region_vars] - log_sum_bf_detect
-
-                            cond_prob = self._convert_huge_log_rel_bf_to_cond_prob(log_rel_bf, max_log)
-                            cond_prob_detect = self._convert_huge_log_rel_bf_to_cond_prob(log_rel_bf_detect, max_log)
+                            (cond_prob, cond_prob_detect) = self._compute_huge_region_conditional_probabilities(
+                                var_log_bf=var_log_bf,
+                                var_log_bf_detect=var_log_bf_detect,
+                                region_vars=region_vars,
+                                max_log=max_log,
+                            )
 
 
                         #this is the final posterior probability of association for all variants in the region
